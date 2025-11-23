aabe6606350e9ab911a89db238246c687a2d2815
IVAN <IVAN.ALEJANDRO.MANCILLA@GMAIL.com>
2025-11-23 03:17:32 -0300

    tabla mas kpis por pala

--- src/app/pages/shovels/components/shovel-info-dialog/shovel-info-dialog.component.ts
+++ src/app/pages/shovels/components/shovel-info-dialog/shovel-info-dialog.component.ts
@@ -3,12 +3,28 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../../environments/environment';
 import { apiIncidentsIncidentByShovelIdShovelGet } from '../../../../api/functions';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { DividerModule } from 'primeng/divider';
+import { TabsModule } from 'primeng/tabs';
+import { TableModule } from 'primeng/table';
+import { TabViewModule } from 'primeng/tabview';
+import { CardModule } from 'primeng/card';
+
+
 
 @Component({
   selector: 'app-shovel-info-dialog',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule,
+    TagModule,
+    TabsModule,
+    TableModule,
+    CardModule,
+    TabsModule,
+    DividerModule,
   ],
   templateUrl: './shovel-info-dialog.component.html',
   styleUrl: './shovel-info-dialog.component.css'
@@ -17,16 +33,19 @@ export class ShovelInfoDialogComponent implements OnInit, OnChanges {
 
   @Input() shovelId?: number;
 
+  test: any = 1;
   incidents: any[] = [];
   loading: boolean = false;
   error: boolean = false;
   rawResponse: any = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     if (this.shovelId) {
       this.loadIncidents(this.shovelId);
+      // grafico
+      //this.generateChart();
 
     }
   }
@@ -42,6 +61,8 @@ export class ShovelInfoDialogComponent implements OnInit, OnChanges {
     this.error = false;
     this.incidents = [];
     this.rawResponse = null;
+    //kpis
+
 
     apiIncidentsIncidentByShovelIdShovelGet(
       this.http,
@@ -64,8 +85,10 @@ export class ShovelInfoDialogComponent implements OnInit, OnChanges {
         }
 
         console.log("INCIDENTES:", this.incidents);
-        this.loading = false;
 
+        this.loading = false;
+        //kpis
+        this.calculateIncidentStats();
       },
 
       error: () => {
@@ -74,4 +97,94 @@ export class ShovelInfoDialogComponent implements OnInit, OnChanges {
       }
     });
   }
+
+  getSeverity(status?: string):
+    'success' | 'secondary' | 'info' | 'warn' | 'danger' | 'contrast' {
+
+    if (!status) return 'contrast';
+
+    const s = status.toLowerCase();
+
+    switch (s) {
+      case 'abierto':
+        return 'warn';
+      case 'aprobado':
+        return 'info';
+      case 'en espera':
+      case 'mantenimiento':
+        return 'secondary';
+      case 'finalizado':
+      case 'online':
+        return 'success';
+      case 'offline':
+        return 'danger';
+      default:
+        return 'contrast';
+    }
+  }
+
+
+  //* kpis */
+
+  incidentStats = {
+    total: 0,
+    abiertos: 0,
+    finalizados: 0,
+    espera: 0,
+    aprobados: 0
+  };
+
+  private calculateIncidentStats() {
+    const list = this.incidents || [];
+
+    this.incidentStats.total = list.length;
+
+    this.incidentStats.abiertos = list.filter(x =>
+      x.statusIncident?.name?.toLowerCase() === 'abierto'
+    ).length;
+
+    this.incidentStats.finalizados = list.filter(x =>
+      x.statusIncident?.name?.toLowerCase() === 'finalizado'
+    ).length;
+
+    this.incidentStats.espera = list.filter(x =>
+      x.statusIncident?.name?.toLowerCase().includes('espera')
+    ).length;
+
+    this.incidentStats.aprobados = list.filter(x =>
+      x.statusIncident?.name?.toLowerCase() === 'aprobado'
+    ).length;
+  }
+
+ /* data: any;
+  options: any;
+
+  //grafico
+  generateChart() {
+  this.data = {
+    labels: ['Total', 'En Espera', 'Abiertos', 'Aprobados', 'Finalizados'],
+    datasets: [
+      {
+        label: 'Incidentes',
+        data: [
+          this.incidentStats.total,
+          this.incidentStats.espera,
+          this.incidentStats.abiertos,
+          this.incidentStats.aprobados,
+          this.incidentStats.finalizados
+        ],
+        fill: false,
+        borderColor: '#3B82F6',
+        tension: 0.3
+      }
+    ]
+  };
+
+  this.options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+}*/
+
+
 }
